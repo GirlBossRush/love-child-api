07f70917aacdd718e29239e5366e30e73cfd64bf
Teffen Ellis (Eric) <teffen@nirri.us>
2014-08-03 21:32:45 +0900

    Clean up CORS headers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,22 +5,28 @@ class ApplicationController < ActionController::Base
   before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
 
+  # --- CORS ---
   def cors_set_access_control_headers
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
-    headers["Access-Control-Allow-Headers"] = "Origin, Content-Type, Accept, Authorization, Token"
-    headers["Access-Control-Max-Age"] = "1728000"
+    headers.reverse_merge!(
+      "Access-Control-Allow-Origin" => "*",
+      "Access-Control-Allow-Methods" => "POST, GET, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers" => "Origin, Content-Type, Accept, Authorization, Token",
+      "Access-Control-Max-Age" => "1728000"
+    )
   end
 
   def cors_preflight_check
     if request.method == "OPTIONS"
-      headers["Access-Control-Allow-Origin"] = "*"
-      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
-      headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With, X-Prototype-Version, Token"
-      headers["Access-Control-Max-Age"] = "1728000"
+      headers.reverse_merge!(
+        "Access-Control-Allow-Origin" => "*",
+        "Access-Control-Allow-Methods" => "POST, GET, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers" => "Content-Type, X-Requested-With, X-Prototype-Version, Token",
+        "Access-Control-Max-Age" => "1728000"
+      )
 
       render nothing: true, status: 200
     end
   end
+  # ---
 
 end
