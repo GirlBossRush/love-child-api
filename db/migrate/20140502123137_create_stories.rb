d525605d130374ba2e8a048eb95c58b2a166be6d
Teffen Ellis (Eric) <teffen@nirri.us>
2014-08-22 19:42:27 +0900

    Squash migrations.

--- db/migrate/20140502123137_create_stories.rb
+++ db/migrate/20140502123137_create_stories.rb
@@ -2,10 +2,14 @@ class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.text :body
 
       t.timestamps
     end
+
+    add_index :stories, :title
+    add_index :stories, :description
+    add_index :stories, :body
   end
 end
